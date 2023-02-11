6f21047723054f70c2f904015f1cdad9a338d722
manggalateknologidotcom <124939783+manggalateknologidotcom@users.noreply.github.com>
2023-02-11 14:30:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
-apt install docker.io -y
-apt install docker-compose -y
+apt install docker-ce -y
