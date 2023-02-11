71ccbed96248527c3ecca550433870667bb52f01
manggalateknologidotcom <124939783+manggalateknologidotcom@users.noreply.github.com>
2023-02-11 11:48:15 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt update
+apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
+apt install docker.io -y
+apt install docker-compose -y
