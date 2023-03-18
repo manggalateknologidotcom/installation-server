eb6bc9cddcc423d51b8585b55d6bd64ab7b1dece
Azhar nian <azharnian@gmail.com>
2023-03-18 08:23:03 +0700

    Update install.sh
    add Swap Vps

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
 apt install docker-ce -y
+
+fallocate -l 5G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness=10' >> /etc/sysctl.conf
+sysctl vm.vfs_cache_pressure=50
+echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
